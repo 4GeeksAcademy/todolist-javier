2907c3ce26636856d5e3c12668e9d98048cb71b7
Javier de los Ángeles <javier.delosangeles@gmail.com>
2025-01-13 10:50:49 +0000

    ejercicio hecho

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div classname="container">
+			<h1>Todos</h1>
+			<ul>
+				<li><input 
+				        type="text"
+				        onChange={(e) => setInputValue(e.target.value)} 
+						value={inputValue} 
+						onKeyDown={(e) => {
+							if (e.key === 'Enter') {
+							  setTodos([...todos, inputValue]);
+							  setInputValue("");
+							}}}
+						placeholder="What needs to be done?">
+					</input>
+				</li>
+				{todos.map((tarea, index) => (
+					<li>
+						{/*Filtro de todos los index menos el que se hace click */}
+						{tarea} <i class="fa-solid fa-trash" onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex ))}></i>
+					</li>
+				))}
+
+			</ul>
+			<div>{todos.length} item left</div>
 		</div>
 	);
 };
